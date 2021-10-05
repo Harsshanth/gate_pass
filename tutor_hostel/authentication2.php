1a18666e25978782adec4177bc9cf71ed9c9f49c
Harsshanth <73587980+Harsshanth@users.noreply.github.com>
2021-10-05 14:38:35 +0530

    init

---
+++ tutor_hostel/authentication2.php
@@ -0,0 +1,22 @@
+<?php      
+    include('connection.php');  
+     
+    $rollno = $_POST['rollno'];  
+      
+
+        $check = stripcslashes($rollno);  
+ 
+        $check = mysqli_real_escape_string($con, $rollno);  
+      
+        $sql = "select *from security where  rollno = '$rollno'";  
+        $result = mysqli_query($con, $sql);  
+
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Permitted </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Not permitted.</h1>";  
+        }     
+?>s
---
+++ tutor_hostel/security.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<form action="authentication2.php" method="POST" >
+<head>
+	<title>security</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<h1><center>Security Check</center></h1>
+	<tr><td>Rollno
+		<input type="integet" name="rollno">
+	</td></tr>
+	<tr>
+	<td><input type="submit" name="check" value="check"></td>
+</tr>
+	
+
+</body>
+</form>
+</html>
